ac434e665101a7f0cd0c03a860627ed811bdd4aa
damasbahagia1 <damasbahagia1@gmail.com>
2024-09-07 08:35:49 +0000

    new animation

--- script.js
+++ script.js
@@ -15,3 +15,49 @@ function CloseMenu(icon) {
     navMenu.classList.remove('opacity-100', 'z-9999');
     navMenu.classList.add('opacity-0', '-z-[9999]');
 }
+
+const slider = document.querySelector('.slider');
+const list = document.querySelector('.list');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+
+// AUTOPLAY
+let runAutoplay = setTimeout(() => {
+    next.click();   
+}, 7000);
+
+
+next.addEventListener('click', () => {
+    initSlider('next');
+});
+
+prev.addEventListener('click', () => {
+    initSlider('prev');
+});
+
+
+
+const initSlider = (type) => {
+    const sliderItems = list.querySelectorAll('.item');
+
+    if(type === 'next') {
+        list.appendChild(sliderItems[0]);
+        slider.classList.add('next');
+    } else {
+        const lastItemPosition = sliderItems.length - 1;
+        list.prepend(sliderItems[lastItemPosition]);
+        slider.classList.add('prev');
+
+    }
+
+    setTimeout(() =>  {
+        slider.classList.remove('next');
+        slider.classList.remove('prev');
+    }, 2000);
+
+
+    clearTimeout(runAutoplay);
+    runAutoplay = setTimeout(() => {
+        next.click();   
+    }, 7000);
+}
